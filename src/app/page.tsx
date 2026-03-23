153ef09c602e1567b0329da50184b3f8bb0ac1c0
Satwick Shaw <satwick1234509@gmail.com>
2026-03-23 18:17:25 +0530

    fix missing images: use profile.png and placeholder for travels

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
         >
           <div className="w-full h-full rounded-full overflow-hidden bg-black">
             <Image
-              src="/image1.jpg"
+              src="/profile.png"
               alt="Satwick Shaw"
               width={224}
               height={224}
